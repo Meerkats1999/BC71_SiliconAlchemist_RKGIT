b2da536baf11558e6dcc334b4d25e52d8b9e6791
yatan.vesh <yatan.vesh@gmail.com>
2020-08-01 19:49:36 +0530

    Baisc display of traffic and reporting done

--- frontend-web/src/App.js
+++ frontend-web/src/App.js
@@ -33,6 +33,118 @@ class App extends React.Component {
     feedbacks: []
   };
 
+  async componentDidMount() {
+    getClientLocationData(this.onDataReceive);
+    getMultiLocationData(this.onMultiDataReceive);
+    getSignalData(this.ongetSignalData);
+    let {feedbacks} = await listFeedback()
+    feedbacks&& this.addFeedbacks(feedbacks);
+    // this.timer = setInterval(()=>{
+    //   if(this.state.simulationState==='RUNNING'){
+    //     let simulationTime = this.state.simulationTime +1;
+    //     this.setState({simulationTime});
+    //   }
+    // },1000);
+  }
+
+  addFeedbacks = (feedbacks) => {
+    const clients = {...this.state.clients};
+    feedbacks.map(feedback => {
+      clients[feedback._id] = {
+        coords: {lat: feedback.lat, lng: feedback.lon},
+        type: 'static',
+        src: feedback.contentURL,
+        id: feedback._id,
+        angle: -90
+      };
+    });
+    this.setState({clients});
+  }
+
+  componentWillMount() {
+    clearInterval(this.timer);
+  }
+
+  handleMiddleware = (signal) => {
+    switch (signal) {
+      case 'start':
+        this.setState({simulationState: 'RUNNING'})
+        break;
+      case 'reset':
+        this.setState({simulationState: 'OFF', simulationTime: 0});
+        break;
+      case 'pause':
+        this.setState({simulationState: 'PAUSED'})
+        break;
+      default:
+        break;
+    }
+    sendSignal(signal);
+  }
+
+  onDataReceive = (data) => {
+    let {clients} = this.state;
+
+    let {id, coords, type} = data;
+    if (id) {
+      clients[id] = {
+        coords,
+        type
+      }
+    }
+    this.setState({clients});
+  };
+
+  onMultiDataReceive = (data) => {
+    this.setState({simulationTime: data.time})
+    const {vehicles} = data;
+    if (!vehicles) {
+      console.log("No vehicles");
+      return;
+    }
+    const {clients} = this.state;
+    // const clients = {};
+
+    vehicles.map(vehicle => {
+      if (vehicle.id) {
+        clients[vehicle.id] = {
+          coords: {
+            lat: vehicle.lat,
+            lng: vehicle.lng
+          },
+          type: vehicle.type,
+          angle: vehicle.angle
+        }
+      }
+    });
+
+    this.setState({clients});
+  }
+
+  ongetSignalData = (data) => {
+    const {signals} = data;
+    // console.log(data)
+    if (!signals) {
+
+      console.log("No Signals");
+      return;
+    }
+    const {trafficSignals} = this.state;
+    signals.map(signal => {
+      if (signal.id) {
+        trafficSignals[signal.id] = {
+          coords: {
+            lat: signal.lat,
+            lng: signal.lng
+          },
+          state: signal.state,
+        }
+      }
+    });
+
+    this.setState({trafficSignals});
+  }
+
   render() {
     return (
       <div className="App">
@@ -61,5 +173,4 @@ export default App;
 // <Chart 
 //           light={'grgfygyrrG'}
 //         />
-
 
