903e711480805b8c88859837b0392877e0ee56ce
Abhrajyoti Pal <abhrajyoti1999@gmail.com>
2020-08-01 23:02:44 +0530

    built and working with darkflow

---
+++ ml-model/traffic-density-cctv/app.py
@@ -0,0 +1,13 @@
+from darkflow.net.build import TFNet
+import numpy as np
+import cv2
+
+
+def makeBoxAndStuff(original_img, predictions):
+    
+    return newImage
+
+options = {"model": "cfg/yolo.cfg", "load": "bin/yolo.weights", "threshold": 0.3}
+
+tfnet = TFNet(options)
+
