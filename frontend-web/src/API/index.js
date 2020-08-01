be6ee9b8fcc398d0316e1158d967a56acb51baa8
yatan.vesh <yatan.vesh@gmail.com>
2020-08-01 12:31:05 +0530

    Added api methods, keys

---
+++ frontend-web/src/API/index.js
@@ -0,0 +1,48 @@
+import io from 'socket.io-client';
+import axios from 'axios';
+const url = 'http://localhost:3002';
+// const url='https://speedwagon-service.herokuapp.com'
+let socket = io.connect(url);
+// let socket=io.connect(url);
+
+export const getClientLocationData = cb => {
+    socket.on('CLIENT_LOCATION', function(data) {
+        console.log('Received CLIENT_LOCATION', data);
+        cb(data);
+    });
+};
+
+export const getMultiLocationData = cb => {
+    socket.on('LOCATIONS', function(data) {
+        // console.log('Received Vehicle LOCATIONS', data);
+        cb(data);
+    });
+};
+
+export const getSignalData = cb =>{
+    socket.on('SIGNALS',function(data){
+        // console.log('Received Signal LOCATIONS', data);
+        cb(data);
+    })
+}
+
+export const sendSignal = async (signal='start') =>{
+    try{
+        
+        socket.emit('CONTROL',signal);
+    }catch(eror){
+        console.log("Eror in signal", eror);
+    }
+}
+
+export const listFeedback = async () => {
+    try {
+        let response = await axios.get(`${url}/feedback/all`);
+        return response.data;
+    } catch (error) {
+        console.log(error);
+        return false;
+    }
+}
+
+export default socket;
--- frontend-web/src/index.js
+++ frontend-web/src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import WebFont from 'webfontloader';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 
 
 import * as serviceWorker from './serviceWorker';
 
 WebFont.load({
     google: {
-        families: ['Titillium Web:300,400,700', 'sans-serif', 'Oswald']
+        families: ['Titillium Web:300,400,700', 'sans-serif', 'Oswald','Righteous']
     }
 });
 
