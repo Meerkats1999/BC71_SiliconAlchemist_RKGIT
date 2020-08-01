ddefc27af606e64dcc7300d72b8ec31ef0be29f8
yatan.vesh <yatan.vesh@gmail.com>
2020-08-01 06:07:36 +0530

    Empty web react project init

---
+++ frontend-web/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './App.css';
+
+class App extends React.Component {
+  render(){
+    return (
+      <div className="App">
+        <h1 className={'Heading'}>TRAFFIC CONTROLLER</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
